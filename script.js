83a9370f80c2ccff2eea69cd50a46d718bf71a33
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-12-10 14:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+btn.addEventListener('click', (e) => {
+	abc();
+})
+
+async function abc() {
+	output.innerText = await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			 resolve(text.value);
+		}, Number(delay.value));
+	})
+}
